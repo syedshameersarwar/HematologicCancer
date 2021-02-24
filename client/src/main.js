bed75f9e10c9ef1224786b39d9d296eea1fbef7b
kazzastic <rkazim79@gmail.com>
2021-02-25 02:29:58 +0500

    adding few frontend components

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Quasar from 'quasar';
+import QFile from 'quasar';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
 }).$mount("#app");
+
+Vue.use(Quasar, {
+  components: {
+    QFile
+  }
+});
