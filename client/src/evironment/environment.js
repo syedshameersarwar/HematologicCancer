e804c44787a8398f25c2bca5782d642e62d4ca58
kazzastic <rkazim79@gmail.com>
2021-02-25 00:08:55 +0500

    api service added

--- client/src/evironment/environment.js
+++ client/src/evironment/environment.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
 
 export const flaskBackendService = axios.create({
-    baseURL: process.env.flaskBackendService,
+    baseURL: "http://127.0.0.1:5000/",
 });
--- client/src/services/test-service.js
+++ client/src/services/test-service.js
@@ -1,6 +1,14 @@
 import api from "../services/api-service";
 
 class TestService {
-
+    testPythonApi() {
+        try {
+            return api.get({
+                url: "flask/get-response",
+            });
+        } catch (error) {
+            return new Error(error);
+        }
+    }
 }
 export default new TestService();
