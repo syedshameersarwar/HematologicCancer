1c25fb204e8ab4fb3b1fa8b445c7669cb5229078
kazzastic <rkazim79@gmail.com>
2021-02-24 20:01:24 +0500

    this is the component format we'll use seperate html css and vue files

---
+++ client/src/components/Header/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template src="./Header.html"></template>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./Header.css" scoped></style>
